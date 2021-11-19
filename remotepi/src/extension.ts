0d248f84a0419c9f705a58467f0c5ea75ca8c447
George <ebelingg@mail.gvsu.edu>
2021-11-19 13:57:19 -0500

    Loosly woorking SSH connect for prototyping

--- remotepi/src/extension.ts
+++ remotepi/src/extension.ts
@@ -1,27 +1,75 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { isIPv4 } from 'net';
 import * as vscode from 'vscode';
+import * as ssh2 from 'ssh2';
+
+
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "remotepi" is now active!');
+
+
+	console.log("VSCode extension remotepi is active");
+
+	const conn = new ssh2.Client(); 
 
 	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('remotepi.build3b+', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showErrorMessage("Nothing was built for the 3b_, I'm a fraud");
+		vscode.window.showErrorMessage("Not implemented");
 	});
 
 	vscode.commands.registerCommand('remotepi.build4', () => {
-		vscode.window.showErrorMessage("The build4 command is current not implemented");
-	
+		vscode.window.showErrorMessage("Not implemented");
+	});
+
+	vscode.commands.registerCommand('remotepi.connectssh', async () => {
+	//https://www.npmjs.com/package/node-ssh
+
+
+		let username = await vscode.window.showInputBox({
+			ignoreFocusOut : true,
+			prompt : 'Username for ssh...',
+			placeHolder : 'Default : pi',
+		});
+
+		let passcode = await vscode.window.showInputBox({
+			ignoreFocusOut: true,
+			prompt: 'Password for ssh...',
+			placeHolder : 'Default : raspberry',
+		});
+
+		let ip = await vscode.window.showInputBox({
+			ignoreFocusOut : true,
+			prompt : 'IPv4 Address of the remote pi...',
+		});
+
+		if(isIPv4(ip as string)){
+			conn.on('ready', () => {
+				console.log('Client :: ready');
+				conn.shell((err, stream) => {
+				  if (err) {throw err;}
+				  stream.on('close', () => {
+					console.log('Stream :: close');
+					conn.end();
+				  }).on('data', (data) => {
+					console.log('OUTPUT: ' + data);
+				  });
+				  stream.end('ls -l\nexit\n');
+				});
+			  }).connect({
+				host: ip as string,
+				username: username as string,
+				port: 22,
+				password: passcode as string,
+				tryKeyboard: true,
+			  });
+			
+		}
+			
+
 	});
 
 	context.subscriptions.push(disposable);
