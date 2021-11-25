ba73fc6a57823cf76bb400fa6cd78fd02f24e37b
ebelingGWEIV <ebelingg@mail.gvsu.edu>
2021-11-25 17:10:23 -0500

    Moved asking for remote info to a function

--- remotepi/src/extension.ts
+++ remotepi/src/extension.ts
@@ -7,78 +7,97 @@ import { Terminal } from 'vscode';
 
 //This that should be settings somewhere
 let sshSetup : boolean = false; //this is only for me <-- This should be persistent per host connection
-
+let termi : Terminal;
+//#todo add support for multiple remote devicdees
+let username : string;
+let passcode : string;
+let ip       : string;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	
-
 	console.log("VSCode extension remotepi is active");
 
-    let termi : Terminal = vscode.window.createTerminal();
-	//#todo add support for multiple remote devicdees
-	let username : string | undefined;
-	let passcode : string | undefined;
-	let ip       : string | undefined;
+	termi = vscode.window.createTerminal();
+
 
 
 	// The command has been defined in the package.json file
 	let disposable = vscode.commands.registerCommand('remotepi.build3b+', () => {
-		vscode.window.showErrorMessage("Not implemented");
+		// termi.sendText("/opt/pi/tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/ \\ bin/arm-linux-gnueabihf-gcc -static -x c - <<EOF #include <stdio.h> int main(void) { printf(\"Hello Cross Compiler!\n\"); return 0;	} EOF");
 	});
-
 	vscode.commands.registerCommand('remotepi.build4', () => {
 		vscode.window.showErrorMessage("Not implemented");
 	});
 
 	vscode.commands.registerCommand("remotepi.setupNewRemote", async () => {
 		
-		username = await vscode.window.showInputBox({
-			ignoreFocusOut : true,
-			prompt : 'Username for ssh...',
-			placeHolder : 'Default : pi',
-		});
-		username = (username?.length === 0 ? "pi" : username);
-
-		passcode = await vscode.window.showInputBox({
-			ignoreFocusOut: true,
-			prompt: 'Password for ssh...',
-			placeHolder : 'Default : raspberry',
-		});
-		passcode = (passcode?.length === 0 ? "978Raspberry.132" : passcode);
-
-		ip = await vscode.window.showInputBox({
-			ignoreFocusOut : true,
-			prompt : 'Hostname of the remote pi...',
-			placeHolder: 'Example : 192.168.137.224'
-		});
-		ip = (ip?.length === 0 ? "192.168.137.224" : ip);
-
-
+		//Check if a remote has been defined
+		//#todo Support more than one remote
+		//#todo Have persistent information
+		if(username.length <= 0 && passcode.length <= 0 && ip.length <= 0) {
+			getRemoteInfo();
+		}
+		
 		if(isIPv4(ip as string) || (ip as string)?.length > 0){ 
 			termi.show();
 			
 			//Setup a passcodeless login
 			if(sshSetup !== true){
-				termi.sendText("echo setting up SSH key...");
-				termi.sendText("ssh-keygen -b 2048 -t rsa -f /tmp/sshkey -q -N \"" + passcode + "\" && ssh-copy-id -i /tmp/sshkey " + username + "@" + ip, false);
+				vscode.window.showInformationMessage("Currently, the secure ssh connection requires user interaction with the terminal");
+				// termi.sendText("echo setting up SSH key...");
+				// termi.sendText("ssh-keygen -b 2048 -t rsa -f /tmp/sshkey -q -N \"" + passcode + "\" && ssh-copy-id -i /tmp/sshkey " + username + "@" + ip );
 				// while(termi.state.isInteractedWith !== true){}; //wait for user interaction to finish the copy <-- This doesn't work, maybe because this command is async
 			}
 		}
 
 	});
 
+	//This is for opening a ssh connection for the user to use. Should not be used for scp
 	vscode.commands.registerCommand('remotepi.connectssh', () => {
+		
+		//#todo Support more than one remote
+		//#todo Have persistent information
+		if(username.length <= 0 && passcode.length <= 0 && ip.length <= 0) {
+			getRemoteInfo();
+		}
+		
 		termi.show();
 		termi.sendText(" ssh " + username + "@" + ip);
-			
-
 	});
+
+
 
 	context.subscriptions.push(disposable);
 }
 
+
+export function getRemoteInfo()
+{
+	username = (vscode.window.showInputBox({
+		ignoreFocusOut : true,
+		prompt : 'Username for ssh...',
+		placeHolder : 'Default : pi',
+	}) as unknown) as string;
+	username = (username?.length === 0 ? "pi" : username);
+
+	passcode = (vscode.window.showInputBox({
+		ignoreFocusOut: true,
+		prompt: 'Password for ssh...',
+		placeHolder : 'Default : raspberry',
+	}) as unknown) as string;
+	passcode = (passcode?.length === 0 ? "978Raspberry.132" : passcode);
+
+	ip = (vscode.window.showInputBox({
+		ignoreFocusOut : true,
+		prompt : 'Hostname of the remote pi...',
+		placeHolder: 'Example : 192.168.137.112'
+	})as unknown) as string;
+	ip = (ip?.length === 0 ? "192.168.137.112" : ip);
+}
+
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	
+}
