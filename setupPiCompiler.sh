40f08daf7bd40b7e3c90d658cf12df5a949ea406
George <37000248+ebelingGWEIV@users.noreply.github.com>
2021-12-09 11:00:02 -0500

    Removed old setup script

--- setupPiCompiler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Download the tool chain for comppiling raspberry pi binaries
-sudo mkdir /opt/pi
-sudo chown $(whoami) /opt/pi
-cd /opt/pi
-git clone https://github.com/raspberrypi/tools
-
